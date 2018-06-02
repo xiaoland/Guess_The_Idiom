4e5f1c1b8a505b922cdf4f3d4267a1e628290a6a
Lanzhijiang <37663413+xiaoland@users.noreply.github.com>
2018-06-02 22:04:49 +0800

    Update rest.py

--- rest.py
+++ rest.py
@@ -3,3 +3,25 @@
 import sys
 import os
 import json
+import web
+import xml.etree.ElementTree as ET
+sys.path.append('/xiaolan/skills/')
+import music
+import news
+import smarthome
+import joke
+import tuling
+import weather
+
+urls=(
+    '/xiaolan/skills/text=(.*)','skills',
+    '/xiaolan/states/start/(.*)','log',
+    '/xiaolan/state/shutdown/(.*)','log',
+    '/xiaolan/data/smarthome/e_id=(.*)&text=(.*)','smarthome',
+)
+app = web.application(urls,globals())
+
+class skills:
+    
+    def GET(self, text):
+        

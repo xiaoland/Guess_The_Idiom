a054aac12f832e07d84e442a90a80782be712675
Lanzhijiang <37663413+xiaoland@users.noreply.github.com>
2018-06-09 23:28:29 +0800

    Create num.py

---
+++ num.py
@@ -0,0 +1,11 @@
+class nu(object):
+    def __init__(self):
+        pass
+    def n(self, number, o):
+        
+        if o == 0:
+            numbers = {'first': number}
+            pass
+        else:
+            return numbers
+    

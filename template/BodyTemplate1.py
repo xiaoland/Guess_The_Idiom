59a530e9b76543b16f97ed32a8d894184705c93f
Your Name <you@example.com>
2018-06-30 13:50:10 +0800

    sdk plus

---
+++ template/BodyTemplate1.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding=utf-8 -*-
+
+# author:sunshaolei
+
+from dueros.directive.BaseDirective import BaseDirective
+
+class BodyTemplate1(BaseDirective):
+    """
+    卡片形式展示
+    """
+
+    def __init__(self):
+        '''
+        文本卡片显示的content
+        :param content:
+        '''
+        super(BodyTemplate1, self).__init__('BodyTemplate1')
+
+
+    def setBackgroundImage(self, url, widthPixels=0, heightPixels=0):
+        self.data["setBackgroundImage"] = {
+            "url": url,
+            "widthPixels": widthPixels,
+            "heightPixels": heightPixels,
+        }
+
+    def setTitle(self, title):
+        self.data["title"] = title
+
+    def setTextContent(self, text):
+        self.data["content"] = {
+            "type": "PlainText",
+            "text": text
+        }
+
+
+

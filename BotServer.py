69b8af5e13887ed9660aa22aceb41de6983517c2
softking <softking.top@gmail.com>
2018-07-03 23:05:06 +0800

    update

--- BotServer.py
+++ BotServer.py
@@ -14,44 +14,42 @@ from dueros.duerskills.skills import guess
 
 def application(environ, start_response):
 
-    try:
-        method = environ.get('REQUEST_METHOD', 'HEAD')
-        if method == "HEAD":
-            response_headers = [('Content-Type', 'application/json'),
+
+    method = environ.get('REQUEST_METHOD', 'HEAD')
+    if method == "HEAD":
+        response_headers = [('Content-Type', 'application/json'),
                                 ('Content-Length', str(len("")))]
-            start_response('200 OK', response_headers)
-            return [""]
-    except:
-        pass
+        start_response('200 OK', response_headers)
+        return [""]
+    else:
+        try:
+            request_body_size = int(environ.get('CONTENT_LENGTH', 0))
+        except(ValueError):
+            request_body_size = 0
+        request_body = environ['wsgi.input'].read(request_body_size).decode('utf-8')
+        print('request_body = %s\n' % request_body)
+        if not request_body:
+            return ['未获取到请求数据']
 
-    try:
-        request_body_size = int(environ.get('CONTENT_LENGTH', 0))
-    except(ValueError):
-        request_body_size = 0
-    request_body = environ['wsgi.input'].read(request_body_size).decode('utf-8')
-    print('request_body = %s\n' % request_body)
-    if not request_body:
-        return ['未获取到请求数据']
+        bot = guess(request_body)
+        #添加错误回调方法
+        bot.setCallBack(callback)
 
-    bot = guess(request_body)
-    #添加错误回调方法
-    bot.setCallBack(callback)
+        #验证签名enableVerifyRequestSign  disableVerifyRequestSign 关闭验证签名
+        # bot.initCertificate(environ).enableVerifyRequestSign()
+        # bot.initCertificate(environ).disableVerifyRequestSign()
 
-    #验证签名enableVerifyRequestSign  disableVerifyRequestSign 关闭验证签名
-    # bot.initCertificate(environ).enableVerifyRequestSign()
-    # bot.initCertificate(environ).disableVerifyRequestSign()
+        body_str = bot.run()
 
-    body_str = bot.run()
+        body = body_str.encode('utf-8')
 
-    body = body_str.encode('utf-8')
+        response_headers = [('Content-Type', 'application/json'),
+                            ('Content-Length', str(len(body)))]
+        status = '200 OK'
 
-    response_headers = [('Content-Type', 'application/json'),
-                        ('Content-Length', str(len(body)))]
-    status = '200 OK'
+        start_response(status, response_headers)
 
-    start_response(status, response_headers)
-
-    return [body]
+        return [body]
 
 
 def callback(data):

9abe19e620564d93581ab04259313ade1275cdd0
softking <softking.top@gmail.com>
2018-07-04 00:33:44 +0800

    update

--- skills.py
+++ skills.py
@@ -45,8 +45,8 @@ class guess(Bot):
              'http://dbp-resource.gz.bcebos.com/509b8811-c1d4-238d-5a0e-1f1b319a9e4b/%E9%87%8D%E8%B9%88%E8%A6%86%E8%BE%99.jpg?authorization=bce-auth-v1%2Fa4d81bbd930c41e6857b989362415714%2F2018-06-27T05%3A32%3A57Z%2F-1%2F%2F313be2b8aed3acd3add552259087cbfd44f94433de25b392b8200b8ff6528566'],
             ['一石二鸟',
              'http://dbp-resource.gz.bcebos.com/509b8811-c1d4-238d-5a0e-1f1b319a9e4b/%E4%B8%80%E7%9F%B3%E4%BA%8C%E9%B8%9F.jpg?authorization=bce-auth-v1%2Fa4d81bbd930c41e6857b989362415714%2F2018-06-27T05%3A32%3A50Z%2F-1%2F%2Fe67712a139d93b7bd0edfd717fbcf5695059157f80762511e49abaa4742ec54d'],
-            ['一叶障目',
-             'http://dbp-resource.gz.bcebos.com/509b8811-c1d4-238d-5a0e-1f1b319a9e4b/%E4%B8%80%E5%8F%B6%E9%9A%9C%E7%9B%AE.jpg?authorization=bce-auth-v1%2Fa4d81bbd930c41e6857b989362415714%2F2018-06-27T05%3A32%3A52Z%2F-1%2F%2F8783bf1fc817a358befcb275862d3f5cf2bca5f6fed635f4b21c7b7f8b69358a'],
+            #['一叶障目',
+            # 'http://dbp-resource.gz.bcebos.com/509b8811-c1d4-238d-5a0e-1f1b319a9e4b/%E4%B8%80%E5%8F%B6%E9%9A%9C%E7%9B%AE.jpg?authorization=bce-auth-v1%2Fa4d81bbd930c41e6857b989362415714%2F2018-06-27T05%3A32%3A52Z%2F-1%2F%2F8783bf1fc817a358befcb275862d3f5cf2bca5f6fed635f4b21c7b7f8b69358a'],
             ['一帆风顺',
              'http://dbp-resource.gz.bcebos.com/509b8811-c1d4-238d-5a0e-1f1b319a9e4b/%E4%B8%80%E5%B8%86%E9%A3%8E%E9%A1%BA.jpg?authorization=bce-auth-v1%2Fa4d81bbd930c41e6857b989362415714%2F2018-06-27T05%3A32%3A51Z%2F-1%2F%2F45464eabffbaed1b21d81230b2936ed879ea96c2ddc291f35377c733a881d0b0'],
             ['愚公移山',
@@ -57,10 +57,10 @@ class guess(Bot):
              'http://dbp-resource.gz.bcebos.com/509b8811-c1d4-238d-5a0e-1f1b319a9e4b/%E6%97%A0%E4%B8%AD%E7%94%9F%E6%9C%89.jpg?authorization=bce-auth-v1%2Fa4d81bbd930c41e6857b989362415714%2F2018-06-27T05%3A32%3A47Z%2F-1%2F%2Ff70f4c40a6c811128d471d8b2f0aee0a0147f0739e0960a0ca129ec5fa1081ae'],
             ['卧薪尝胆',
              'http://dbp-resource.gz.bcebos.com/509b8811-c1d4-238d-5a0e-1f1b319a9e4b/%E5%8D%A7%E8%96%AA%E5%B0%9D%E8%83%86.jpg?authorization=bce-auth-v1%2Fa4d81bbd930c41e6857b989362415714%2F2018-06-27T05%3A32%3A46Z%2F-1%2F%2F2a0d0502972e94ca2f21b7dd905510f04eadb8e14d2054950c0643a1b59f64bf'],
-            ['身怀六甲',
-             'http://dbp-resource.gz.bcebos.com/509b8811-c1d4-238d-5a0e-1f1b319a9e4b/%E8%BA%AB%E6%80%80%E5%85%AD%E7%94%B2.jpg?authorization=bce-auth-v1%2Fa4d81bbd930c41e6857b989362415714%2F2018-06-27T05%3A30%3A08Z%2F-1%2F%2Fe013c083b1a7ae4fc6976e8ac79c81c7940a24b9b49e096006475a8867b1d499'],
-            ['全心投入',
-             'http://dbp-resource.gz.bcebos.com/509b8811-c1d4-238d-5a0e-1f1b319a9e4b/%E5%85%A8%E5%BF%83%E6%8A%95%E5%85%A5.jpg?authorization=bce-auth-v1%2Fa4d81bbd930c41e6857b989362415714%2F2018-06-27T05%3A30%3A00Z%2F-1%2F%2F65d7fb04d32c2d64abb134ad8be345c4bd81a32bdfe8071dc56d19da7c9de5f0'],
+            #['身怀六甲',
+            # 'http://dbp-resource.gz.bcebos.com/509b8811-c1d4-238d-5a0e-1f1b319a9e4b/%E8%BA%AB%E6%80%80%E5%85%AD%E7%94%B2.jpg?authorization=bce-auth-v1%2Fa4d81bbd930c41e6857b989362415714%2F2018-06-27T05%3A30%3A08Z%2F-1%2F%2Fe013c083b1a7ae4fc6976e8ac79c81c7940a24b9b49e096006475a8867b1d499'],
+            #['全心投入',
+            # 'http://dbp-resource.gz.bcebos.com/509b8811-c1d4-238d-5a0e-1f1b319a9e4b/%E5%85%A8%E5%BF%83%E6%8A%95%E5%85%A5.jpg?authorization=bce-auth-v1%2Fa4d81bbd930c41e6857b989362415714%2F2018-06-27T05%3A30%3A00Z%2F-1%2F%2F65d7fb04d32c2d64abb134ad8be345c4bd81a32bdfe8071dc56d19da7c9de5f0'],
             ['如雷贯耳',
              'http://dbp-resource.gz.bcebos.com/509b8811-c1d4-238d-5a0e-1f1b319a9e4b/%E5%A6%82%E9%9B%B7%E8%B4%AF%E8%80%B3.jpg?authorization=bce-auth-v1%2Fa4d81bbd930c41e6857b989362415714%2F2018-06-27T05%3A30%3A02Z%2F-1%2F%2F33bebb1510bcc9c083a92794702178f332ff48ce0a19c228d66addd70e4455c5'],
             ['石破天惊',
@@ -79,8 +79,8 @@ class guess(Bot):
              'http://dbp-resource.gz.bcebos.com/509b8811-c1d4-238d-5a0e-1f1b319a9e4b/%E4%B8%83%E7%AA%8D%E7%94%9F%E7%83%9F.jpg?authorization=bce-auth-v1%2Fa4d81bbd930c41e6857b989362415714%2F2018-06-27T05%3A29%3A54Z%2F-1%2F%2Fea42310ed43a139fe8baeb108b90c768767ca1e153fa65e9e9f084f46bdf0e35'],
             ['明镜高悬',
              'http://dbp-resource.gz.bcebos.com/509b8811-c1d4-238d-5a0e-1f1b319a9e4b/%E6%98%8E%E9%95%9C%E9%AB%98%E6%82%AC.jpg?authorization=bce-auth-v1%2Fa4d81bbd930c41e6857b989362415714%2F2018-06-27T05%3A29%3A47Z%2F-1%2F%2Fac824816d9538ee7e75d84090a0d9688f6c78f1ff56f72e39851c3a8335796c3'],
-            ['妙语如珠',
-             'http://dbp-resource.gz.bcebos.com/509b8811-c1d4-238d-5a0e-1f1b319a9e4b/%E5%A6%99%E8%AF%AD%E5%A6%82%E7%8F%A0.jpg?authorization=bce-auth-v1%2Fa4d81bbd930c41e6857b989362415714%2F2018-06-27T05%3A29%3A46Z%2F-1%2F%2Feb2f5f8846de8803d4366d04f8f720556ba5f25d540d1f8ddd2ec62b73036ad6'],
+            #['妙语如珠',
+            # 'http://dbp-resource.gz.bcebos.com/509b8811-c1d4-238d-5a0e-1f1b319a9e4b/%E5%A6%99%E8%AF%AD%E5%A6%82%E7%8F%A0.jpg?authorization=bce-auth-v1%2Fa4d81bbd930c41e6857b989362415714%2F2018-06-27T05%3A29%3A46Z%2F-1%2F%2Feb2f5f8846de8803d4366d04f8f720556ba5f25d540d1f8ddd2ec62b73036ad6'],
             ['目不识丁',
              'http://dbp-resource.gz.bcebos.com/509b8811-c1d4-238d-5a0e-1f1b319a9e4b/%E7%9B%AE%E4%B8%8D%E8%AF%86%E4%B8%81.jpg?authorization=bce-auth-v1%2Fa4d81bbd930c41e6857b989362415714%2F2018-06-27T05%3A29%3A48Z%2F-1%2F%2F454b61cd89235a7248857bb018c3927f82b94bb8ba19de6ecc7e318247f5a7b3'],
             ['水滴石穿',
@@ -91,12 +91,12 @@ class guess(Bot):
              'http://dbp-resource.gz.bcebos.com/509b8811-c1d4-238d-5a0e-1f1b319a9e4b/%E7%A0%B4%E5%8F%A3%E5%A4%A7%E9%AA%82.jpg?authorization=bce-auth-v1%2Fa4d81bbd930c41e6857b989362415714%2F2018-06-27T05%3A29%3A51Z%2F-1%2F%2F0e0eb242713965db213344402015c6a49bef9d16ddd03a540fa7fab859e07701'],
             ['天外有天',
              'http://dbp-resource.gz.bcebos.com/509b8811-c1d4-238d-5a0e-1f1b319a9e4b/%E5%A4%A9%E5%A4%96%E6%9C%89%E5%A4%A9.jpg?authorization=bce-auth-v1%2Fa4d81bbd930c41e6857b989362415714%2F2018-06-27T05%3A30%3A33Z%2F-1%2F%2Fe22e790e3cf25550a93f7a78cc1bfd257c12b8bc1442edac9546055b7ca07797'],
-            ['表里如一',
-             'http://dbp-resource.gz.bcebos.com/509b8811-c1d4-238d-5a0e-1f1b319a9e4b/%E8%A1%A8%E9%87%8C%E5%A6%82%E4%B8%80.jpg?authorization=bce-auth-v1%2Fa4d81bbd930c41e6857b989362415714%2F2018-06-26T15%3A16%3A12Z%2F-1%2F%2Fba70cfca1477e9d77f3a73c43e3ed4a647b7cecf68859d0e6370ef2dc0b89d22'],
+            #['表里如一',
+            # 'http://dbp-resource.gz.bcebos.com/509b8811-c1d4-238d-5a0e-1f1b319a9e4b/%E8%A1%A8%E9%87%8C%E5%A6%82%E4%B8%80.jpg?authorization=bce-auth-v1%2Fa4d81bbd930c41e6857b989362415714%2F2018-06-26T15%3A16%3A12Z%2F-1%2F%2Fba70cfca1477e9d77f3a73c43e3ed4a647b7cecf68859d0e6370ef2dc0b89d22'],
             ['杀鸡取卵',
              'http://dbp-resource.gz.bcebos.com/509b8811-c1d4-238d-5a0e-1f1b319a9e4b/%E6%9D%80%E9%B8%A1%E5%8F%96%E5%8D%B5.jpg?authorization=bce-auth-v1%2Fa4d81bbd930c41e6857b989362415714%2F2018-06-27T05%3A30%3A07Z%2F-1%2F%2F2c674d176675072db9dfede60d6d316822743b3a2aa6773a598c902fe5e433f8'],
-            ['横冲直撞',
-             'http://dbp-resource.gz.bcebos.com/509b8811-c1d4-238d-5a0e-1f1b319a9e4b/%E6%A8%AA%E5%86%B2%E7%9B%B4%E6%92%9E.jpg?authorization=bce-auth-v1%2Fa4d81bbd930c41e6857b989362415714%2F2018-06-26T15%3A20%3A02Z%2F-1%2F%2Fdd7f7b6f5456f85602b63b515d5badf171280dd2fe2c49ae3c0db19bf27dafb0'],
+            #['横冲直撞',
+            # 'http://dbp-resource.gz.bcebos.com/509b8811-c1d4-238d-5a0e-1f1b319a9e4b/%E6%A8%AA%E5%86%B2%E7%9B%B4%E6%92%9E.jpg?authorization=bce-auth-v1%2Fa4d81bbd930c41e6857b989362415714%2F2018-06-26T15%3A20%3A02Z%2F-1%2F%2Fdd7f7b6f5456f85602b63b515d5badf171280dd2fe2c49ae3c0db19bf27dafb0'],
             ['前赴后继',
              'http://dbp-resource.gz.bcebos.com/509b8811-c1d4-238d-5a0e-1f1b319a9e4b/%E5%89%8D%E4%BB%86%E5%90%8E%E7%BB%A7.jpg?authorization=bce-auth-v1%2Fa4d81bbd930c41e6857b989362415714%2F2018-06-27T05%3A29%3A52Z%2F-1%2F%2F8caba59d3f424f80d8b87546d4bc85213160b9423b9efa758cd6b6b10aa56e5a'],
             ['对牛弹琴',
@@ -125,9 +125,9 @@ class guess(Bot):
              'http://dbp-resource.gz.bcebos.com/509b8811-c1d4-238d-5a0e-1f1b319a9e4b/%E6%B2%A7%E6%B5%B7%E4%B8%80%E7%B2%9F.jpg?authorization=bce-auth-v1%2Fa4d81bbd930c41e6857b989362415714%2F2018-06-26T15%3A16%3A14Z%2F-1%2F%2Feda993f9eedf385d94dcb67f8719ff51e375e153f7ea5af3ed31b39a29a4cf98'],
             ['一目十行',
              'http://dbp-resource.gz.bcebos.com/509b8811-c1d4-238d-5a0e-1f1b319a9e4b/%E4%B8%80%E7%9B%AE%E5%8D%81%E8%A1%8C.jpg?authorization=bce-auth-v1%2Fa4d81bbd930c41e6857b989362415714%2F2018-06-27T05%3A32%3A49Z%2F-1%2F%2F63cb1996c32fa54b72add290c718e5e11ce4c7fe1a3b3adefc5ff2cdbd5cdd54'],
-            ['只手遮天',
-             'http://dbp-resource.gz.bcebos.com/509b8811-c1d4-238d-5a0e-1f1b319a9e4b/%E5%8F%AA%E6%89%8B%E9%81%AE%E5%A4%A9.jpg?authorization=bce-auth-v1%2Fa4d81bbd930c41e6857b989362415714%2F2018-06-27T05%3A32%3A54Z%2F-1%2F%2F8d587d062352728df5e210e3f0a3ad7e391ae66221bdc02ba41f81504152bcc0'],
-            ['坐吃空山',
+            #['只手遮天',
+            # 'http://dbp-resource.gz.bcebos.com/509b8811-c1d4-238d-5a0e-1f1b319a9e4b/%E5%8F%AA%E6%89%8B%E9%81%AE%E5%A4%A9.jpg?authorization=bce-auth-v1%2Fa4d81bbd930c41e6857b989362415714%2F2018-06-27T05%3A32%3A54Z%2F-1%2F%2F8d587d062352728df5e210e3f0a3ad7e391ae66221bdc02ba41f81504152bcc0'],
+            ['坐吃山空',
              'http://dbp-resource.gz.bcebos.com/509b8811-c1d4-238d-5a0e-1f1b319a9e4b/%E5%9D%90%E5%90%83%E5%B1%B1%E7%A9%BA.jpg?authorization=bce-auth-v1%2Fa4d81bbd930c41e6857b989362415714%2F2018-06-27T05%3A32%3A57Z%2F-1%2F%2F8a83010d48f201c71b1049e02b5b145ae13fefba0244f197ac2ce8ccafbf4349'],
             ['心直口快',
              'http://dbp-resource.gz.bcebos.com/509b8811-c1d4-238d-5a0e-1f1b319a9e4b/%E5%BF%83%E7%9B%B4%E5%8F%A3%E5%BF%AB.jpg?authorization=bce-auth-v1%2Fa4d81bbd930c41e6857b989362415714%2F2018-06-27T05%3A32%3A47Z%2F-1%2F%2Fce3334dec0e828fbcab4cee988526635173a303edf36dc2679083058508bb89a'],
@@ -159,8 +159,8 @@ class guess(Bot):
              'http://dbp-resource.gz.bcebos.com/509b8811-c1d4-238d-5a0e-1f1b319a9e4b/%E6%97%A5%E4%B8%8A%E4%B8%89%E7%AB%BF.jpg?authorization=bce-auth-v1%2Fa4d81bbd930c41e6857b989362415714%2F2018-06-27T05%3A30%3A01Z%2F-1%2F%2Feaf21f7ee5637d36cff6e6636a2b9f163602c322a8c5cc8bf348350787efaa72'],
             ['青黄不接',
              'http://dbp-resource.gz.bcebos.com/509b8811-c1d4-238d-5a0e-1f1b319a9e4b/%E9%9D%92%E9%BB%84%E4%B8%8D%E6%8E%A5.jpg?authorization=bce-auth-v1%2Fa4d81bbd930c41e6857b989362415714%2F2018-06-27T05%3A29%3A55Z%2F-1%2F%2F4a5d6e341a421743125de239c8047b132e7e831158eff132db642064828d14fb'],
-            ['逆流而上',
-             'http://dbp-resource.gz.bcebos.com/509b8811-c1d4-238d-5a0e-1f1b319a9e4b/%E9%80%86%E6%B5%81%E8%80%8C%E4%B8%8A.jpg?authorization=bce-auth-v1%2Fa4d81bbd930c41e6857b989362415714%2F2018-06-27T05%3A29%3A48Z%2F-1%2F%2Fd2264cddce9aeba607142fd3011e6a09435687fb08c8ce1ff00bdbb1985aa0c3'],
+            #['逆流而上',
+            # 'http://dbp-resource.gz.bcebos.com/509b8811-c1d4-238d-5a0e-1f1b319a9e4b/%E9%80%86%E6%B5%81%E8%80%8C%E4%B8%8A.jpg?authorization=bce-auth-v1%2Fa4d81bbd930c41e6857b989362415714%2F2018-06-27T05%3A29%3A48Z%2F-1%2F%2Fd2264cddce9aeba607142fd3011e6a09435687fb08c8ce1ff00bdbb1985aa0c3'],
             ['军令如山',
              'http://dbp-resource.gz.bcebos.com/509b8811-c1d4-238d-5a0e-1f1b319a9e4b/%E5%86%9B%E4%BB%A4%E5%A6%82%E5%B1%B1.jpg?authorization=bce-auth-v1%2Fa4d81bbd930c41e6857b989362415714%2F2018-06-26T15%3A20%3A13Z%2F-1%2F%2F5c0948f070e42fb39cb562e66a3c4cbefc294276bc93e99971487f9d10a8c808'],
             ['开门见山',
@@ -189,8 +189,8 @@ class guess(Bot):
              'http://dbp-resource.gz.bcebos.com/509b8811-c1d4-238d-5a0e-1f1b319a9e4b/%E9%A3%9E%E9%BB%84%E8%85%BE%E8%BE%BE.jpg?authorization=bce-auth-v1%2Fa4d81bbd930c41e6857b989362415714%2F2018-06-26T15%3A17%3A52Z%2F-1%2F%2F8663cb4ba342405d2d4a1370204d45ada555b74d5eea69c368ce29331a44cd05'],
             ['半壁江山',
              'http://dbp-resource.gz.bcebos.com/509b8811-c1d4-238d-5a0e-1f1b319a9e4b/%E5%8D%8A%E5%A3%81%E6%B1%9F%E5%B1%B1.jpg?authorization=bce-auth-v1%2Fa4d81bbd930c41e6857b989362415714%2F2018-06-26T15%3A16%3A14Z%2F-1%2F%2Ffc8c49911bedc5adf814e57bb38a5342fc7c94d303555735878798629d9d19d0'],
-            ['八面来风',
-             'http://dbp-resource.gz.bcebos.com/509b8811-c1d4-238d-5a0e-1f1b319a9e4b/%E5%85%AB%E9%9D%A2%E6%9D%A5%E9%A3%8E.jpg?authorization=bce-auth-v1%2Fa4d81bbd930c41e6857b989362415714%2F2018-06-26T15%3A16%3A09Z%2F-1%2F%2Fb48665ddc3e0fd00062cb0b20d932a1ab1a13054eb5f4b32c4443862fb5eceab'],
+            #['八面来风',
+            # 'http://dbp-resource.gz.bcebos.com/509b8811-c1d4-238d-5a0e-1f1b319a9e4b/%E5%85%AB%E9%9D%A2%E6%9D%A5%E9%A3%8E.jpg?authorization=bce-auth-v1%2Fa4d81bbd930c41e6857b989362415714%2F2018-06-26T15%3A16%3A09Z%2F-1%2F%2Fb48665ddc3e0fd00062cb0b20d932a1ab1a13054eb5f4b32c4443862fb5eceab'],
             ['百里挑一',
              'http://dbp-resource.gz.bcebos.com/509b8811-c1d4-238d-5a0e-1f1b319a9e4b/%E7%99%BE%E9%87%8C%E6%8C%91%E4%B8%80.jpg?authorization=bce-auth-v1%2Fa4d81bbd930c41e6857b989362415714%2F2018-06-26T15%3A16%3A11Z%2F-1%2F%2Fed5e0e6d3d42ac90aa31f83b9ec9da7f2a6c16da95acdb8e23623e4bd7e7a73b'],
             ['不分彼此',
@@ -207,14 +207,14 @@ class guess(Bot):
              'http://dbp-resource.gz.bcebos.com/509b8811-c1d4-238d-5a0e-1f1b319a9e4b/%E5%8D%95%E6%9E%AA%E5%8C%B9%E9%A9%AC.jpg?authorization=bce-auth-v1%2Fa4d81bbd930c41e6857b989362415714%2F2018-06-26T15%3A16%3A21Z%2F-1%2F%2F5c09472e857c985d4c248dc34d5bef947ca6aad9d58676fb9459f2dfe05d9cf9'],
             ['德高望重',
              'http://dbp-resource.gz.bcebos.com/509b8811-c1d4-238d-5a0e-1f1b319a9e4b/%E5%BE%B7%E9%AB%98%E6%9C%9B%E9%87%8D.jpg?authorization=bce-auth-v1%2Fa4d81bbd930c41e6857b989362415714%2F2018-06-26T15%3A16%3A48Z%2F-1%2F%2F4ee7fda0f622fbfe8166f9ab226578f29023a1d3c6b1ddbff593ce8705dac3f0'],
-            ['大跌眼镜',
-             'http://dbp-resource.gz.bcebos.com/509b8811-c1d4-238d-5a0e-1f1b319a9e4b/%E5%A4%A7%E8%B7%8C%E7%9C%BC%E9%95%9C.jpg?authorization=bce-auth-v1%2Fa4d81bbd930c41e6857b989362415714%2F2018-06-26T15%3A16%3A21Z%2F-1%2F%2F704c9f4a0c16b64117dacc2b343ce0dfe85159008016105743fbced16eb132ae'],
-            ['楚河汉界',
-             'http://dbp-resource.gz.bcebos.com/509b8811-c1d4-238d-5a0e-1f1b319a9e4b/%E6%A5%9A%E6%B1%89%E6%B2%B3%E7%95%8C.jpg?authorization=bce-auth-v1%2Fa4d81bbd930c41e6857b989362415714%2F2018-06-26T15%3A16%3A19Z%2F-1%2F%2Fcf58a296afbd0bb1e0ad0076cb6476701d8ba9a9a06bad407abf573ebf9fca92'],
+            #['大跌眼镜',
+            # 'http://dbp-resource.gz.bcebos.com/509b8811-c1d4-238d-5a0e-1f1b319a9e4b/%E5%A4%A7%E8%B7%8C%E7%9C%BC%E9%95%9C.jpg?authorization=bce-auth-v1%2Fa4d81bbd930c41e6857b989362415714%2F2018-06-26T15%3A16%3A21Z%2F-1%2F%2F704c9f4a0c16b64117dacc2b343ce0dfe85159008016105743fbced16eb132ae'],
+            #['楚河汉界',
+            # 'http://dbp-resource.gz.bcebos.com/509b8811-c1d4-238d-5a0e-1f1b319a9e4b/%E6%A5%9A%E6%B1%89%E6%B2%B3%E7%95%8C.jpg?authorization=bce-auth-v1%2Fa4d81bbd930c41e6857b989362415714%2F2018-06-26T15%3A16%3A19Z%2F-1%2F%2Fcf58a296afbd0bb1e0ad0076cb6476701d8ba9a9a06bad407abf573ebf9fca92'],
             ['朝三暮四',
              'http://dbp-resource.gz.bcebos.com/509b8811-c1d4-238d-5a0e-1f1b319a9e4b/%E6%9C%9D%E4%B8%89%E6%9A%AE%E5%9B%9B.jpg?authorization=bce-auth-v1%2Fa4d81bbd930c41e6857b989362415714%2F2018-06-26T15%3A16%3A16Z%2F-1%2F%2F73075286eac90e9e66a1b9b36a2bfb6301dae9bdbcf4f6173a567b3ddac10f24'],
-            ['嫦娥奔月',
-             'http://dbp-resource.gz.bcebos.com/509b8811-c1d4-238d-5a0e-1f1b319a9e4b/%E5%AB%A6%E5%A8%A5%E5%A5%94%E6%9C%88.jpg?authorization=bce-auth-v1%2Fa4d81bbd930c41e6857b989362415714%2F2018-06-26T15%3A16%3A15Z%2F-1%2F%2F5400d4ae0070c9472aa79102b2b494eb3cdcaaaac1451efb465315c0cc09535b'],
+            #['嫦娥奔月',
+            # 'http://dbp-resource.gz.bcebos.com/509b8811-c1d4-238d-5a0e-1f1b319a9e4b/%E5%AB%A6%E5%A8%A5%E5%A5%94%E6%9C%88.jpg?authorization=bce-auth-v1%2Fa4d81bbd930c41e6857b989362415714%2F2018-06-26T15%3A16%3A15Z%2F-1%2F%2F5400d4ae0070c9472aa79102b2b494eb3cdcaaaac1451efb465315c0cc09535b'],
             ['绝处逢生',
              'http://dbp-resource.gz.bcebos.com/509b8811-c1d4-238d-5a0e-1f1b319a9e4b/%E7%BB%9D%E5%A4%84%E9%80%A2%E7%94%9F.jpg?authorization=bce-auth-v1%2Fa4d81bbd930c41e6857b989362415714%2F2018-06-26T15%3A20%3A11Z%2F-1%2F%2Fedb62519bbd9758e9008f7d89c145f5e1e54a955a0c33d11a73cc4db45e27acd']
         ]
@@ -251,7 +251,7 @@ class guess(Bot):
 
         # ------------- fix by sunshaolei -------
 
-        pos = random.randint(0, 87)
+        pos = random.randint(0, 75)
 
         self.setSessionAttribute("pos", pos, 0)  # 存储当前正在出现的图片的答案
         self.setSessionAttribute("error_num", 0, 0)  # 存储当前使用者错误次数
@@ -345,7 +345,7 @@ class guess(Bot):
 
             else:
 
-                pos = random.randint(0, 87)
+                pos = random.randint(0, 75)
 
                 self.setSessionAttribute("guanqia_num", guanqia_num + 1, 0)  # 关卡加一
                 self.setSessionAttribute("lun_num", lun_num, 0)
@@ -383,7 +383,7 @@ class guess(Bot):
 
                 else:
 
-                    new_pos = random.randint(0, 87)
+                    new_pos = random.randint(0, 75)
                     self.setSessionAttribute("pos", new_pos, 0)
                     self.setSessionAttribute("lerror_num", 0, 0)
                     card = ImageCard()
@@ -400,7 +400,7 @@ class guess(Bot):
                 if lerror_num > 2:
                     self.setSessionAttribute("guanqia_num", int(guanqia_num) + 1, 0)  # 关卡加一
 
-                    new_pos = random.randint(0, 87)
+                    new_pos = random.randint(0, 75)
                     self.setSessionAttribute("pos", new_pos, 0)
                     self.setSessionAttribute("lerror_num", 0, 0)
                     self.setSessionAttribute("error_num", error_num + 1, 0)
@@ -445,7 +445,7 @@ class guess(Bot):
 
     def cidiom(self):
 
-        rand_ids = random.randint(0, 87)
+        rand_ids = random.randint(0, 75)
         guanqia_num = int(self.getSessionAttribute("guanqia_num", 0))
         lun_num = int(self.getSessionAttribute("lun_num", 0))
         error_num = int(self.getSessionAttribute("error_num", 0))
@@ -473,7 +473,7 @@ class guess(Bot):
 
     def nidiom(self):
 
-        rand_ids = random.randint(0, 87)
+        rand_ids = random.randint(0, 75)
         lun_num = int(self.getSessionAttribute("lun_num", 0))
         self.setSessionAttribute("guanqia_num", 1, 0)
         self.setSessionAttribute("error_num", 0, 0)
